f5122e1962c75b328931b79d955955720128a445
njausteve <njaustevedomino@gmail.com>
2024-09-14 12:59:02 +0800

    feat: update to elixir 1.17

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExPass.MixProject do
     [
       app: :ex_pass,
       version: "0.1.0",
-      elixir: "~> 1.16",
+      elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
