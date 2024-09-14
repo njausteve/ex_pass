6e5b5a5ef923e9dde07b05a4ce6f737b2a660733
njausteve <njaustevedomino@gmail.com>
2024-09-14 11:56:55 +0800

    chore: Add initial ex_pass project files and configuration

---
+++ ex_pass/lib/ex_pass.ex
@@ -0,0 +1,18 @@
+defmodule ExPass do
+  @moduledoc """
+  Documentation for `ExPass`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExPass.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
