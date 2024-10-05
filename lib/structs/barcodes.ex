53282f21f9c2f8b52e6e7b614e25e0a845a57532
njausteve <njaustevedomino@gmail.com>
2024-10-05 21:39:25 +0800

    docs: update full list of supported IANA strings

--- lib/structs/barcodes.ex
+++ lib/structs/barcodes.ex
@@ -19,7 +19,16 @@ defmodule ExPass.Structs.Barcodes do
 
      Supported formats include:
       - US-ASCII
-      - ISO-8859-1 to ISO-8859-10
+      - ISO-8859-1
+      - ISO-8859-2
+      - ISO-8859-3
+      - ISO-8859-4
+      - ISO-8859-5
+      - ISO-8859-6
+      - ISO-8859-7
+      - ISO-8859-8
+      - ISO-8859-9
+      - ISO-8859-10
       - Shift_JIS
       - EUC-JP
       - ISO-2022-KR
