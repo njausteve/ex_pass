295e851aae41e9204cf2c99be6586e4a2b441f9a
njausteve <njaustevedomino@gmail.com>
2024-10-05 21:30:38 +0800

    feat: Add support for validating message encoding in Barcodes struct

--- lib/structs/barcodes.ex
+++ lib/structs/barcodes.ex
@@ -13,6 +13,29 @@ defmodule ExPass.Structs.Barcodes do
   - `format`: Required. The format of the barcode. Possible values are: PKBarcodeFormatQR, PKBarcodeFormatPDF417, PKBarcodeFormatAztec, PKBarcodeFormatCode128.
     Note: The barcode format PKBarcodeFormatCode128 isn't supported for watchOS.
   - `message`: Required. The message or payload to display as a barcode.
+  - `message_encoding`: Optional. The text encoding used for the message. This should be a valid IANA
+     character set name. The system uses this to convert the message
+     from a string to the data representation rendered as a barcode.
+
+     Supported formats include:
+      - US-ASCII
+      - ISO-8859-1 to ISO-8859-10
+      - Shift_JIS
+      - EUC-JP
+      - ISO-2022-KR
+      - EUC-KR
+      - ISO-2022-JP
+      - ISO-2022-JP-2
+      - ISO-8859-6-E
+      - ISO-8859-6-I
+      - ISO-8859-8-E
+      - ISO-8859-8-I
+      - GB2312
+      - Big5
+      - KOI8-R
+
+    For a complete list, refer to the IANA Character Sets registry at
+    https://www.iana.org/assignments/character-sets/character-sets.xhtml.
   """
 
   use TypedStruct
@@ -24,6 +47,7 @@ defmodule ExPass.Structs.Barcodes do
     field :alt_text, String.t()
     field :format, String.t(), enforce: true
     field :message, String.t(), enforce: true
+    field :message_encoding, String.t(), enforce: true
   end
 
   @doc """
@@ -31,16 +55,42 @@ defmodule ExPass.Structs.Barcodes do
 
   ## Parameters
 
-    * `attrs` - A map of attributes for the Barcodes struct.
+    * `attrs` - A map of attributes for the Barcodes struct. The map can include the following keys:
+      * `:alt_text` - (Optional) Text displayed near the barcode. For example, a human-readable version of the barcode data.
+      * `:format` - (Required) The format of the barcode. Must be one of: "PKBarcodeFormatQR", "PKBarcodeFormatPDF417", "PKBarcodeFormatAztec", or "PKBarcodeFormatCode128".
+      * `:message` - (Required) The message or payload to display as a barcode.
+      * `:message_encoding` - (Required) The text encoding used for the message. Must be a valid IANA character set name.
 
   ## Returns
 
-    * A new Barcodes struct.
+    * A new `%Barcodes{}` struct.
 
   ## Examples
 
-      iex> Barcodes.new(%{alt_text: "Scan this QR code", format: "PKBarcodeFormatQR", message: "123456789"})
-      %Barcodes{alt_text: "Scan this QR code", format: "PKBarcodeFormatQR", message: "123456789"}
+      iex> Barcodes.new(%{
+      ...>   alt_text: "Scan this QR code",
+      ...>   format: "PKBarcodeFormatQR",
+      ...>   message: "https://example.com/ticket/123456789",
+      ...>   message_encoding: "ISO-8859-1"
+      ...> })
+      %Barcodes{
+        alt_text: "Scan this QR code",
+        format: "PKBarcodeFormatQR",
+        message: "https://example.com/ticket/123456789",
+        message_encoding: "ISO-8859-1"
+      }
+
+      iex> Barcodes.new(%{
+      ...>   format: "PKBarcodeFormatPDF417",
+      ...>   message: "PASS-1234-5678-90",
+      ...>   message_encoding: "US-ASCII"
+      ...> })
+      %Barcodes{
+        alt_text: nil,
+        format: "PKBarcodeFormatPDF417",
+        message: "PASS-1234-5678-90",
+        message_encoding: "US-ASCII"
+      }
 
   """
   @spec new(map()) :: %__MODULE__{}
@@ -51,6 +101,7 @@ defmodule ExPass.Structs.Barcodes do
       |> validate(:alt_text, &Validators.validate_optional_string(&1, :alt_text))
       |> validate(:format, &Validators.validate_barcode_format/1)
       |> validate(:message, &Validators.validate_required_string(&1, :message))
+      |> validate(:message_encoding, &Validators.validate_message_encoding/1)
 
     struct!(__MODULE__, attrs)
   end
