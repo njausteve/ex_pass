f007711af2d3149d4f8b70b24d5d4f6860aabcf2
Shijith K <shijithjayan@gmail.com>
2024-10-21 23:24:18 +0530

    feat: :sparkles: Add Seat struct and validation logic with tests
    - Introduced a new `Seat` struct for representing seating info for transit journeys or events. It includes an optional `seat_type` field, validating it as a string. Implemented JSON encoding for the struct, ensuring camelCase formatting. Added comprehensive tests to verify correct struct creation, validation behavior, and JSON encoding. This change enhances data handling consistency for seat information within the application.

---
+++ lib/structs/semantic_tags/seat.ex
@@ -0,0 +1,77 @@
+defmodule ExPass.Structs.SemanticTags.Seat do
+  @moduledoc """
+  Represents  the identification of a seat for a transit journey or an event.
+
+  ## Fields
+
+  * `:seat_type` - Optional. The type of seat, such as “Reserved seating”.
+
+  ## Compatibility
+
+  - iOS 12.0+
+  - iPadOS 6.0+
+  - watchOS 2.0+
+  """
+
+  use TypedStruct
+
+  alias ExPass.Utils.Converter
+  alias ExPass.Utils.Validators
+
+  typedstruct do
+    field :seat_type, String.t()
+  end
+
+  @doc """
+  Creates a new Seat struct.
+
+  ## Parameters
+
+    * `attrs` - A map of attributes for the Locations struct. The map can include the following keys:
+      * `:seat_type` - (Optional) The type of seat, such as “Reserved seating”.
+
+  ## Returns
+
+    * A new `%Seat{}` struct.
+
+  ## Examples
+
+      iex> Seat.new(%{seat_type: "Reserved seating"})
+      %Seat{seat_type: "Reserved seating"}
+
+      iex> Seat.new(%{seat_type: 123})
+      ** (ArgumentError) seat_type must be a string
+
+  """
+  @spec new(map()) :: %__MODULE__{}
+  def new(attrs \\ %{}) do
+    attrs =
+      attrs
+      |> Converter.trim_string_values()
+      |> validate(:seat_type, &Validators.validate_optional_string(&1, :seat_type))
+
+    struct!(__MODULE__, attrs)
+  end
+
+  defp validate(attrs, key, validator) do
+    case validator.(attrs[key]) do
+      :ok ->
+        attrs
+
+      {:error, reason} ->
+        raise ArgumentError, reason
+    end
+  end
+
+  defimpl Jason.Encoder do
+    def encode(seat, opts) do
+      seat
+      |> Map.from_struct()
+      |> Enum.reduce(%{}, fn
+        {_k, nil}, acc -> acc
+        {k, v}, acc -> Map.put(acc, Converter.camelize_key(k), v)
+      end)
+      |> Jason.Encode.map(opts)
+    end
+  end
+end
